795599628f10eab716f58eb60757f5989a93972c
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-07-05 19:40:00 +0200

    modified: skills section with style

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,9 +1,64 @@
+import "./Skills.css";
 const Skills = () => {
-    return (
-        <div>
-            Skills
-        </div>
-    )
-}
+  return (
+    <section id="skills" className="SectionSlice is-sticky SliceSkills">
+      <ul className="skills__list">
+        <li className="list__item">
+          <p className="item__title">HTML-CSS</p>
+          <div className="item__desc">
+            <h4>Building Templates</h4>
+            <p>
+              Using the latest features in HTML5 such as (semantic elements) and
+              CSS3 such as (flex box) to build responsive and dynamic UI
+            </p>
+          </div>
+        </li>
+        <li className="list__item">
+          <p className="item__title">JavaScript(ES6)</p>
+          <div className="item__desc">
+            <h4>Ecmascript 6</h4>
+            <p>Building advanced applications and games using the latest features such as ( Higher order functions, closure and OOP )</p>
+          </div>
+        </li>
+        <li className="list__item">
+          <p className="item__title">Bootstrap</p>
+          <div className="item__desc">
+            <p>This is a description of html css card want to show on hover</p>
+          </div>
+        </li>
+        <li className="list__item">
+          <p className="item__title">ReactJs</p>
+          <div className="item__desc">
+            <p>This is a description of html css card want to show on hover</p>
+          </div>
+        </li>
+        <li className="list__item">
+          <p className="item__title">Git-GitHub</p>
+          <div className="item__desc">
+            <p>This is a description of html css card want to show on hover</p>
+          </div>
+        </li>
+        <li className="list__item">
+          <p className="item__title">Jest</p>
+          <div className="item__desc">
+            <p>This is a description of html css card want to show on hover</p>
+          </div>
+        </li>
+        <li className="list__item">
+          <p className="item__title">TypeScript</p>
+          <div className="item__desc">
+            <p>This is a description of html css card want to show on hover</p>
+          </div>
+        </li>
+        <li className="list__item">
+          <p className="item__title">UI/Ux</p>
+          <div className="item__desc">
+            <p>This is a description of html css card want to show on hover</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+};
 
 export default Skills;
