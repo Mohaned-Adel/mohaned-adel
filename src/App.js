2c9dea63969caf0fa3a9c4aa639dfd879cba487e
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-07-05 14:13:07 +0200

    building project structure with components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+// import Nav from './components/nav/Nav'
+import Header from './components/header/Header';
+import Introduction from './components/introduction/Introduction';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import References from './components/references/References';
+import Projects from './components/projects/Projects';
+import Contact from './components/contact/Contact';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello React
+    <div>
+      {/* <Nav /> */}
+      <Header />
+      <main>
+        <Introduction />
+        <About />
+        <Skills />
+        <References />
+        <Projects />
+        <Contact />
+      </main>
     </div>
   );
 }
