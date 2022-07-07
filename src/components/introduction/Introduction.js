9b802b2a196c3b17a559df6c9e783fc9bfc59ca5
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-07-07 13:52:49 +0200

    modified: References sction with style

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -4,7 +4,7 @@ import IMG from '../../assets/images/main-img.jpg'
 const Introduction = () => {
     return (
         <section id="introduction" className="SectionSlice is-sticky image">
-            <img src={IMG} className="section__image" />
+            <img src={IMG} className="section__image" alt='My Personal img' />
         </section>
     )
 }
--- src/components/references/References.js
+++ src/components/references/References.js
@@ -1,8 +1,35 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination  } from "swiper";
+
+import './References.css'
 const References = () => {
     return (
-        <div>
-            References
-        </div>
+        <section id="#references" className="SectionSlice is-sticky SliceRef">
+            <Swiper className="ref__content"
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{clickable: true}}>
+                <SwiperSlide className="ref__article">
+                    <h3>Building Games</h3>
+                    <p>Advanced Experience using JavaScript concepts such as (Higher order functions, callback functions and closures). Also having a good understanding of JavaScript ES6 features such as (Arrow functions, destructuring, Template literals and Promises). Using all of that to build amazing and awesome games and applications</p>
+                </SwiperSlide>
+                <SwiperSlide className="ref__article">
+                    <h3>Creating Apps</h3>
+                    <p>Advanced Experience using ReactJs concepts such as (class and functional components, React Router and Hooks). Also having a good understanding of Redux. Using all of that to build amazing and awesome applications and Templates</p>
+                </SwiperSlide>
+                <SwiperSlide className="ref__article">
+                    <h3>Templates Ninja</h3>
+                    <p>Creating responsive websites using CSS, Flexbox and CSS Grid, develop interactive websites and UI (User Interface) applications using Bootstrap, SASS, JavaScript and HTML</p>
+                </SwiperSlide>
+            </Swiper>
+        </section>
     )
 }
 
